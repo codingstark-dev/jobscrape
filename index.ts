8cfb51ddee2a3b3ed34360f2e4668aba4da9a290
“Himanshu” <mhimanshu.338@gmail.com>
2024-07-05 20:51:53 +0530

    url updated

--- index.ts
+++ index.ts
@@ -2,8 +2,9 @@ import * as cheerio from "cheerio";
 import { mkdir } from "node:fs/promises";
 import { dirname, join } from "node:path";
 const response = await fetch(
-  "https://www.foundit.in/search/work-from-home-jobs?searchId=3f5e9fcd-2807-4d6a-8835-4c2b89b466fa"
+  "https://www.foundit.in/search/sql-jobs?searchId=8b34f9fd-3deb-4910-be49-3febf4e67861"
 );
+// "https://www.foundit.in/search/work-from-home-jobs?searchId=3f5e9fcd-2807-4d6a-8835-4c2b89b466fa"
 
 const file = Bun.file("./data/jobs.json");
 const data = await file.json();
